5b22774f2de10cedbd087fc34ba8948e94e5932d
benscott <ben@benscott.co.uk>
2015-03-03 14:29:55 +0000

    namespace

--- ckanext/gbif/__init__.py
+++ ckanext/gbif/__init__.py
@@ -5,6 +5,15 @@ Created by 'bens3' on 2013-06-21.
 Copyright (c) 2013 'bens3'. All rights reserved.
 """
 
+# this is a namespace package
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
+    
+
 DQI_UNKNOWN = None
 DQI_NA = 'N/A'
 DQI_MAJOR_ERRORS = 'Major errors'
