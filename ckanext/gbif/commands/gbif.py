6df210aef914993627328ee55ecf497ec79d6330
benscott <ben@benscott.co.uk>
2015-03-04 15:43:22 +0000

    rerun again

--- ckanext/gbif/commands/gbif.py
+++ ckanext/gbif/commands/gbif.py
@@ -20,7 +20,7 @@ NotAuthorized = logic.NotAuthorized
 ValidationError = logic.ValidationError
 
 BATCH_SIZE = 100  # Commit every x number of rows
-COMMIT_FROM = 1769000  # If set, only commit past x number of rows (use if there's been an error)
+COMMIT_FROM = 2027000  # If set, only commit past x number of rows (use if there's been an error)
 
 class GBIFCommand(CkanCommand):
     """
