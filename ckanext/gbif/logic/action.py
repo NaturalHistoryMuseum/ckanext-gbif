b6fabdf9bda9d2fbfc1decc68670b6bf67b8ff84
Alice Butcher <alycejenni@gmail.com>
2018-04-11 11:07:17 +0100

    manual changes to logic

--- ckanext/gbif/logic/action.py
+++ ckanext/gbif/logic/action.py
@@ -4,13 +4,9 @@
 # This file is part of ckanext-gbif
 # Created by the Natural History Museum in London, UK
 
-import pylons
-import ckan.logic as logic
-from ckanext.datastore.db import _get_engine
-from ckanext.datastore.db import _execute_single_statement
+from ckanext.datastore.db import _execute_single_statement, _get_engine
 
-_get_or_bust = logic.get_or_bust
-NotFound = logic.NotFound
+from ckan.plugins import toolkit
 
 
 def gbif_record_show(context, data_dict):
@@ -21,13 +17,16 @@ def gbif_record_show(context, data_dict):
         - occurrence_id
 
     '''
-    occurrence_id = _get_or_bust(data_dict, u'occurrence_id')
+    occurrence_id = toolkit.get_or_bust(data_dict, u'occurrence_id')
     # Set up DB connection to datastore
-    context[u'connection'] = _get_engine({u'connection_url': pylons.config[u'ckan.datastore.read_url']}).connect()
+    context[u'connection'] = _get_engine({
+        u'connection_url': toolkit.config[
+            u'ckan.datastore.read_url']
+        }).connect()
     sql = u'SELECT * FROM gbif WHERE occurrenceid=%s LIMIT 1'
     result = _execute_single_statement(context, sql, occurrence_id)
     record = result.fetchone()
     if record:
         return dict(record)
     else:
-        raise NotFound
+        raise toolkit.ObjectNotFound
