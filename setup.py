0bdc8a3adce2be01340e937952c4afccf2326e09
Alice Butcher <alycejenni@gmail.com>
2018-04-11 11:10:01 +0100

    tidy setup.py + changed version number

--- setup.py
+++ setup.py
@@ -4,29 +4,30 @@
 # This file is part of ckanext-gbif
 # Created by the Natural History Museum in London, UK
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-version = u'0.1'
+version = u'0.2'
 
 setup(
-	name=u'ckanext-gbif',
-	version=version,
-	description=u'GBIF',
-	long_description=u'''GBIF Data Quality indicators''',
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords=u'',
-	license=u'',
-	packages=find_packages(exclude=[u'ez_setup', u'examples', u'tests']),
-	namespace_packages=[u'ckanext', u'ckanext.gbif'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		u'requests',
-		u'python-dateutil'
-	],
-	entry_points=\
-	u'''
-        [ckan.plugins]
-            gbif = ckanext.gbif.plugin:GBIFPlugin
-	''',
-)
+    name=u'ckanext-gbif',
+    version=version,
+    description=u'GBIF',
+    long_description=u'''GBIF Data Quality indicators''',
+    classifiers=[],
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords=u'',
+    license=u'',
+    packages=find_packages(exclude=[u'ez_setup', u'examples', u'tests']),
+    namespace_packages=[u'ckanext', u'ckanext.gbif'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        u'requests',
+        u'python-dateutil'
+        ],
+    entry_points= \
+        u'''
+            [ckan.plugins]
+                gbif = ckanext.gbif.plugin:GBIFPlugin
+        ''',
+    )
