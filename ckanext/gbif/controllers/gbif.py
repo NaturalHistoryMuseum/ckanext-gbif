97050e5fa1c52e0e1da94aa33cb3763cd3fa443e
Alice Butcher <alycejenni@gmail.com>
2018-04-11 11:04:38 +0100

    manual changes to controllers

--- ckanext/gbif/controllers/gbif.py
+++ ckanext/gbif/controllers/gbif.py
@@ -4,29 +4,16 @@
 # This file is part of ckanext-gbif
 # Created by the Natural History Museum in London, UK
 
-import pylons
 import logging
-import ckan.logic as logic
-import ckan.lib.base as base
-import ckan.model as model
-import ckan.plugins as p
-from ckan.common import _, c
-from ckan.plugins import toolkit as tk
+
+from ckan.plugins import toolkit
 
 log = logging.getLogger(__name__)
 
-render = base.render
-abort = base.abort
-redirect = base.redirect
 
-NotFound = logic.NotFound
-NotAuthorized = logic.NotAuthorized
-ValidationError = logic.ValidationError
-get_action = logic.get_action
-
-
-class GBIFController(p.toolkit.BaseController):
+class GBIFController(toolkit.BaseController):
     '''Controller for displaying about pages'''
+
     def view(self, package_name, resource_id, record_id):
         '''
 
@@ -36,37 +23,47 @@ class GBIFController(p.toolkit.BaseController):
 
         '''
 
-        context = {u'model': model, u'session': model.Session, u'user': c.user or c.author}
+        context = {
+            u'user': toolkit.c.user or toolkit.c.author
+            }
 
         # Try & get the resource
         try:
-            c.resource = get_action(u'resource_show')(context, {u'id': resource_id})
-            c.package = get_action(u'package_show')(context, {u'id': package_name})
-            c.pkg_dict = c.package
-            record = get_action(u'record_show')(context, {u'resource_id': resource_id, u'record_id': record_id})
-            c.record_dict = record[u'data']
+            toolkit.c.resource = toolkit.get_action(u'resource_show')(context, {
+                u'id': resource_id
+                })
+            toolkit.c.package = toolkit.get_action(u'package_show')(context, {
+                u'id': package_name
+                })
+            toolkit.c.pkg_dict = toolkit.c.package
+            record = toolkit.get_action(u'record_show')(context, {
+                u'resource_id': resource_id,
+                u'record_id': record_id
+                })
+            toolkit.c.record_dict = record[u'data']
 
-        except NotFound:
-            abort(404, _(u'Resource not found'))
-        except NotAuthorized:
-            abort(401, _(u'Unauthorized to read resource %s') % package_name)
+        except toolkit.ObjectNotFound:
+            toolkit.abort(404, toolkit._(u'Resource not found'))
+        except toolkit.NotAuthorized:
+            toolkit.abort(401,
+                          toolkit._(u'Unauthorized to read resource %s') % package_name)
 
-        occurrence_id = c.record_dict.get(u'occurrenceID')
+        occurrence_id = toolkit.c.record_dict.get(u'occurrenceID')
 
         if not occurrence_id:
-            abort(404, _(u'GBIF record not found'))
+            toolkit.abort(404, toolkit._(u'GBIF record not found'))
 
         # And get the GBIF record
         try:
-            gbif_record = tk.get_action(u'gbif_record_show')(context, {
+            gbif_record = toolkit.get_action(u'gbif_record_show')(context, {
                 u'occurrence_id': occurrence_id
-            })
-        except NotFound:
-            abort(404, _(u'GBIF record not found'))
+                })
+        except toolkit.ObjectNotFound:
+            toolkit.abort(404, toolkit._(u'GBIF record not found'))
         else:
-            return render(u'record/gbif.html', {
+            return toolkit.render(u'record/gbif.html', {
                 u'title': u'GBIF',
                 u'gbif_record': gbif_record,
-                u'organisation_key': pylons.config[u'ckanext.gbif.organisation_key'],
-                u'dataset_key': pylons.config[u'ckanext.gbif.dataset_key']
-            })
+                u'organisation_key': toolkit.config[u'ckanext.gbif.organisation_key'],
+                u'dataset_key': toolkit.config[u'ckanext.gbif.dataset_key']
+                })
